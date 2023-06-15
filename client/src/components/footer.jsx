1d37b21bdfea298ae924190fc5af5dd5c1ee83ec
sachariase99 <126781566+sachariase99@users.noreply.github.com>
2023-06-15 15:36:07 +0200

    Update footer.jsx

--- client/src/components/footer.jsx
+++ client/src/components/footer.jsx
@@ -22,7 +22,7 @@ const Footer = () => {
                 </article>
                 <article>
                     <h3 className='text-blue'>Google Maps:</h3>
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2170.4592382643536!2d9.913872460159405!3d57.04367940569943!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x464932f3bcda26e3%3A0xcd44aae1c87a63c5!2sBoulevarden%2039%2C%209000%20Aalborg!5e0!3m2!1sda!2sdk!4v1686641313271!5m2!1sda!2sdk" title='map' width="100%" height="250" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" className='p-5 pb-0'></iframe>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2170.4592382643536!2d9.913872460159405!3d57.04367940569943!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x464932f3bcda26e3%3A0xcd44aae1c87a63c5!2sBoulevarden%2039%2C%209000%20Aalborg!5e0!3m2!1sda!2sdk!4v1686641313271!5m2!1sda!2sdk" title='map' width="100%" height="250" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </article>
             </section>
         </footer>
